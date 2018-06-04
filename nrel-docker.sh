68f600371295d075f7d2fa0c831cfb76eec8fe41
bbest <ben@ecoquants.com>
2018-06-04 11:24:53 -0700

    working

--- nrel-docker.sh
+++ nrel-docker.sh
@@ -4,21 +4,21 @@ DIR_SHINY_LOG="/Users/bbest/docker-nrel/shiny-log"
 DIR_GITHUB="/Users/bbest/docker-nrel/github"
 DIR_DATA="/Users/bbest/docker-nrel/data"
 PASSWD="/Users/bbest/.passwd"
+DIR_TMP="/tmp"
 
 # set variables
 USER="ben"
 TAG_DOCKER="bdbest/rstudio-shiny:R-3.5-geospatial"
-
-# load app files from Github repo
-git clone "https://github.com/ecoquants/nrel-uses.git" "$DIR_GITHUB/nrel-uses"
+DATA_ZIP_URL="https://drive.google.com/a/ecoquants.com/uc?id=1tdUmwJWYKAPc2iHJeavLh_Iyn8BmMeHH&export=download"
 
 # if need to recreate docker container:
 # docker stop rstudio-shiny; docker rm rstudio-shiny
 
 # run docker, downloading image if needed
+# bump memory to 8 GB for global.R `s_constraints = stack(constraints_grd)`
 docker run --name "rstudio-shiny" \
   --restart unless-stopped \
-  -p 8787:8787 -p 3838:3838 \
+  -p 8787:8787 -p 80:3838 \
   -v $DIR_DATA:/data \
   -v $DIR_GITHUB:/github \
   -v $DIR_SHINY_APPS:/srv/shiny-server \
@@ -27,14 +27,22 @@ docker run --name "rstudio-shiny" \
   -e USER=$USER -e PASSWORD=`cat $PASSWD` \
   -d -t $TAG_DOCKER
 
+# load app files from Github repo
+git clone "https://github.com/ecoquants/nrel-uses.git" "$DIR_GITHUB/nrel-uses"
+
+# load data from Google Drive/projects/nrel-ecoquants/data/app-data.zip
+wget -O $DIR_TMP/z.$$ $DATA_ZIP_URL && 
+   unzip -d $DIR_DATA $DIR_TMP/z.$$ &&
+   rm $DIR_TMP/z.$$
+
 alias de="docker exec -i -t 'rstudio-shiny'"
+
+# to quick install without rebuild for future...
+PACKAGES="c('here','glue')"
+de Rscript -e "install.packages($PACKAGES, repos = 'http://cran.us.r-project.org')"
 
 # add symlink: github/nrel-uses/app -> apps/uses
 de ln -s "/github/nrel-uses/app" "/srv/shiny-server/uses"
-
-# to quick install without rebuild for future...
-#PACKAGES="c('leaflet','shinydashboard')"
-#de -i -t "rstudio-shiny" Rscript -e "install.packages($PACKAGES, repos = 'http://cran.us.r-project.org')"
 
 # Add symlinks for ease of navigation in RStudio
 de ln -s "/data" "/home/$USER/data"; \
--- rstudio-shiny/README.md
+++ rstudio-shiny/README.md
@@ -11,19 +11,15 @@ To do this, composed a Dockerfile from geoserver R version with Shiny tag:
 
 - added other needed packages: `leaflet`, `shinydashboard`
 
-- `docker build` image, which takes ~ 3 hours:
+- `docker build` image, which takes ~ 1.5 hrs initially, 15 min for updates:
 
   ```bash
+  # docker rmi bdbest/rstudio-shiny:R-3.5-geospatial; \  
   date; \
   docker build -t bdbest/rstudio-shiny:R-3.5-geospatial \
     "https://github.com/ecoquants/nrel-docker.git#master:rstudio-shiny"; \
   date
   ```
-  on BB's Mac: 1.5 hr
-  round 2 with FROM rocker/geospatial: 11 min
-  round 3:
-  - Sat Jun  2 18:57:05 PDT 2018
-  - 
 
 - `docker login` and `docker push` image
 
@@ -31,34 +27,3 @@ To do this, composed a Dockerfile from geoserver R version with Shiny tag:
   docker login # registed as bdbest (bdbest@gmail.com)
   docker push "bdbest/rstudio-shiny:R-3.5-geospatial"
   ```
-
-## Errors
-
-installing to /usr/local/lib/R/site-library/httpuv/libs
-** R
-** demo
-** preparing package for lazy loading
-** help
-*** installing help indices
-** building package indices
-** testing if installed package can be loaded
-* DONE (httpuv)
-* installing *source* package ‘shiny’ ...
-** package ‘shiny’ successfully unpacked and MD5 sums checked
-** R
-** inst
-** preparing package for lazy loading
-** help
-*** installing help indices
-** building package indices
-** testing if installed package can be loaded
-Error: package or namespace load failed for ‘shiny’:
- .onLoad failed in loadNamespace() for 'shiny', details:
-  call: .Call("_httpuv_getRNGState", PACKAGE = "httpuv")
-  error: "_httpuv_getRNGState" not available for .Call() for package "httpuv"
-Error: loading failed
-Execution halted
-ERROR: loading failed
-* removing ‘/usr/local/lib/R/site-library/shiny’
-* restoring previous ‘/usr/local/lib/R/site-library/shiny’
-  
